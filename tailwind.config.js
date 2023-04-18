b8d67de79c3a74c332a673f7af33170e68d21af5
shunya hayashi <hayashun0226@icloud.com>
2023-04-19 02:23:20 +0900

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "float-up": {
+          "0%": {
+            transform: "translateY(5%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "float-up": "float-up 0.3s ease-out",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
